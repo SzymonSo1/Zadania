a8c50cd7cd44625713010a71cb80f0ce6a3e432c
DelusionalMikuFan <97904365+SzymonSo1@users.noreply.github.com>
2025-02-21 19:23:57 +0100

    Update main.c

--- kalkulator/main.c
+++ kalkulator/main.c
@@ -2,22 +2,34 @@
 
 extern int add(int a, int b);
 extern int sub(int a, int b);
+
 int liczba1;
 int liczba2;
+char wybor = '+';
+
 int main() {
     start();
-    char wybor;
+    int wynik;
 
-    scanf("%c", &wybor);
     wypiszPobranieLiczby();
     getNumbs();
-    if (wybor == '+'){
-        
-    }else if(wybor=='-'){
 
+    if (wybor == '+')
+    {
+        wynik = add(liczba1,liczba2);
+        printf("%d wynik dodania to:", wynik);
+    }else if(wybor=='-')
+    {
+        wynik = sub(liczba1, liczba2);
+        printf("%d wynik odejmowania to: ", wynik);
+    }else
+    {
+        printf("Niepoprawny input");
     }
+
     return 0;
 }
+
 void getNumbs(){
     scanf("%d", &liczba1);
     scanf("%d", &liczba2);
